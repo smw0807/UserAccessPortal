cab28e0bd5d387337be07e3b8a303e7bb58ca79e
smw0807 <smw0807@gmail.com>
2024-10-27 11:12:17 +0900

    alert, confirm 적용

--- layouts/login.vue
+++ layouts/login.vue
@@ -1,7 +1,12 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Alert from '~/components/dialog/Alert.vue';
+import Confirm from '~/components/dialog/Confirm.vue';
+</script>
 <template>
   <v-app>
     <v-main class="d-flex align-center justify-center">
+      <Alert />
+      <Confirm />
       <slot />
     </v-main>
   </v-app>
