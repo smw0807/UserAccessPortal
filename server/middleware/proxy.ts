a4a6d2fc7f6b1204f53fb9dd516bd7af301160ad
smw0807 <smw0807@gmail.com>
2024-11-02 11:55:39 +0900

    주석 제거

--- server/middleware/proxy.ts
+++ server/middleware/proxy.ts
@@ -1,6 +1,5 @@
 //https://jongmin4943.tistory.com/entry/Nuxt3-proxy-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0feat-Spring-API-%EC%84%9C%EB%B2%84
 export default defineEventHandler((event) => {
-  console.log('event.node.req.url : ', event.node.req.url);
   if (!event.node.req.url?.startsWith('/api/')) return;
 
   const config = useRuntimeConfig();
