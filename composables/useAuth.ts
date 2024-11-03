1b6b96d8f134b9f3cba1fe4180b02eabcbe98f17
smw0807 <smw0807@gmail.com>
2024-11-03 20:06:15 +0900

    컴포저블 에러 수정

--- composables/useAuth.ts
+++ composables/useAuth.ts
@@ -1,31 +1,32 @@
 import type { Token } from '~/store/auth';
 
-export default () => {
+export default function useAuth() {
   const accessTokenName = 'user-access-hub-access-token';
   const refreshTokenName = 'user-access-hub-refresh-token';
+  const cookie = useCookie(accessTokenName);
+  const refreshCookie = useCookie(refreshTokenName);
+
   return {
-    // accessTokenName,
-    // refreshTokenName,
-    // 토큰 셋팅
     setToken: (token: Token) => {
-      useCookie(accessTokenName).value = token.access_token;
-      useCookie(refreshTokenName).value = token.refresh_token;
+      if (process.client) {
+        cookie.value = token.access_token;
+        refreshCookie.value = token.refresh_token;
+      }
     },
-    // 토큰 가져오기
     getToken: () => {
       return {
-        access_token: useCookie(accessTokenName).value,
-        refresh_token: useCookie(refreshTokenName).value,
+        access_token: cookie.value,
+        refresh_token: refreshCookie.value,
       };
     },
-    // 토큰 존재 여부 확인
     hasToken: () => {
-      return !!useCookie(accessTokenName).value;
+      return !!cookie.value;
     },
-    // 토큰 삭제
     removeToken: () => {
-      useCookie(accessTokenName).value = '';
-      useCookie(refreshTokenName).value = '';
+      if (process.client) {
+        cookie.value = '';
+        refreshCookie.value = '';
+      }
     },
   };
-};
+}
