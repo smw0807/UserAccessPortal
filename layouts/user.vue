a37cbab6db49e70359537d919a2c193667583810
smw0807 <smw0807@gmail.com>
2024-11-05 20:54:03 +0900

    권한이 USER인 계정 보여줄 페이지

---
+++ layouts/user.vue
@@ -0,0 +1,14 @@
+<!-- ROLE : USER LAYOUT -->
+<script setup lang="ts">
+import Alert from '~/components/dialog/Alert.vue';
+import Confirm from '~/components/dialog/Confirm.vue';
+</script>
+<template>
+  <v-app>
+    <v-main class="d-flex align-center justify-center">
+      <Alert />
+      <Confirm />
+      <slot />
+    </v-main>
+  </v-app>
+</template>
---
+++ pages/user.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: 'user',
+});
+</script>
+<template>
+  <div>user</div>
+</template>
