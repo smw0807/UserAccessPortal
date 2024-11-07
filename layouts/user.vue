ccf517be23ea23efbf8c0c2ae17baca46e880e5e
smw0807 <smw0807@gmail.com>
2024-11-07 20:20:58 +0900

    토큰 없으면 로그인 페이지로

--- layouts/user.vue
+++ layouts/user.vue
@@ -2,6 +2,11 @@
 <script setup lang="ts">
 import Alert from '~/components/dialog/Alert.vue';
 import Confirm from '~/components/dialog/Confirm.vue';
+const { hasToken } = useAuth();
+const router = useRouter();
+if (!hasToken()) {
+  router.replace('/login');
+}
 </script>
 <template>
   <v-app>
