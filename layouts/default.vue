e00114f2bd42a234aec70879f769840561ae2216
smw0807 <smw0807@gmail.com>
2024-11-07 20:20:47 +0900

    일반 사용자일 경우 사용자 페이지로 이동

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import Alert from '~/components/dialog/Alert.vue';
 import Confirm from '~/components/dialog/Confirm.vue';
+import { useUserStore } from '~/store/user';
 const { hasToken } = useAuth();
 const router = useRouter();
 if (!hasToken()) {
   router.replace('/login');
+}
+// 어드민 유저 체크
+const { checkAdminUser } = useUserStore();
+
+const isAdmin = await checkAdminUser();
+if (!isAdmin) {
+  router.replace('/user');
 }
 </script>
 <template>
