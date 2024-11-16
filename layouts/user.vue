32f8cf32a3635f0b7f5fc46859bf5958c6704923
smw0807 <smw0807@gmail.com>
2024-11-16 21:34:57 +0900

    관리자면 루트 페이지로

--- layouts/user.vue
+++ layouts/user.vue
@@ -2,10 +2,17 @@
 <script setup lang="ts">
 import Alert from '~/components/dialog/Alert.vue';
 import Confirm from '~/components/dialog/Confirm.vue';
+import { useUserStore } from '~/store/user';
 const { hasToken } = useAuth();
 const router = useRouter();
 if (!hasToken()) {
   router.replace('/login');
+}
+const userStore = useUserStore();
+
+const isAdmin = await userStore.checkAdminUser();
+if (isAdmin) {
+  router.replace('/');
 }
 </script>
 <template>
