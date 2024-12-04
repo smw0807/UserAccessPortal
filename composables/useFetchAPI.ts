8f12042beef3372848057969e561507ed4d7482c
smw0807 <smw0807@gmail.com>
2024-12-04 16:47:46 +0900

    로그 주석처리

--- composables/useFetchAPI.ts
+++ composables/useFetchAPI.ts
@@ -4,11 +4,11 @@ export default (url: string, options?: UseFetchOptions<any>) => {
   return useFetch(url, {
     ...options,
     onRequest({ request, options }) {
-      console.log('request : ', request);
-      console.log('options : ', options);
+      // console.log('request : ', request);
+      // console.log('options : ', options);
     },
     onResponse({ response }) {
-      console.log('response : ', response);
+      // console.log('response : ', response);
     },
   });
 };
