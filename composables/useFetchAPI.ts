327712d9537686154603531e29aff1da07e1614c
smw0807 <smw0807@gmail.com>
2024-12-04 17:02:39 +0900

    테스트

--- composables/useFetchAPI.ts
+++ composables/useFetchAPI.ts
@@ -7,8 +7,15 @@ export default (url: string, options?: UseFetchOptions<any>) => {
       // console.log('request : ', request);
       // console.log('options : ', options);
     },
+    onRequestError({ request, options }) {
+      // console.log('request error : ', request);
+      // console.log('options error : ', options);
+    },
     onResponse({ response }) {
       // console.log('response : ', response);
+    },
+    onResponseError({ response }) {
+      // console.log('response error : ', response);
     },
   });
 };
