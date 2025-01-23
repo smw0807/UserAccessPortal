eba95f049664c9ae064d09f90906a4d9246d2446
smw0807 <smw0807@gmail.com>
2025-01-23 22:21:30 +0900

    미들웨어 작업으로 인한 주석처리

--- layouts/user.vue
+++ layouts/user.vue
@@ -2,18 +2,18 @@
 <script setup lang="ts">
 import Alert from '~/components/dialog/Alert.vue';
 import Confirm from '~/components/dialog/Confirm.vue';
-import { useUserStore } from '~/store/user';
-const { hasToken } = useAuth();
-const router = useRouter();
-if (!hasToken()) {
-  router.replace('/login');
-}
-const userStore = useUserStore();
+// import { useUserStore } from '~/store/user';
+// const { hasToken } = useAuth();
+// const router = useRouter();
+// if (!hasToken()) {
+//   router.replace('/login');
+// }
+// const userStore = useUserStore();
 
-const isAdmin = await userStore.checkAdminUser();
-if (isAdmin) {
-  router.replace('/');
-}
+// const isAdmin = await userStore.checkAdminUser();
+// if (isAdmin) {
+//   router.replace('/');
+// }
 </script>
 <template>
   <v-app>
