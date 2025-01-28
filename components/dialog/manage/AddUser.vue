986c7843f553b2d7e748d9b9c81b0c027dde227d
smw0807 <smw0807@gmail.com>
2025-01-28 14:17:53 +0900

    회원추가 컴포넌트

--- components/dialog/manage/AddUser.vue
+++ components/dialog/manage/AddUser.vue
@@ -1,13 +1,75 @@
 <script setup lang="ts">
+import { roleItems, statusItems } from '~/constants/item';
+const emit = defineEmits(['addUser']);
 const open = ref(false);
 const handleOpen = () => (open.value = true);
 const handleClose = () => (open.value = false);
+
+const form = ref();
+const email = ref('');
+const name = ref('');
+const phoneNumber = ref('');
+const role = ref('');
+const status = ref('');
+
+const rules = useRules();
+const { useConfirm } = useDialog();
+const handleAddUser = async () => {
+  const { valid } = await form.value.validate();
+  if (!valid) return;
+  const confirm = await useConfirm({
+    title: '회원 추가',
+    type: 'warning',
+    message: '회원을 추가하시겠습니까?',
+    okText: '추가',
+    cancelText: '취소',
+  });
+  if (confirm) {
+    emit('addUser', {
+      email: email.value,
+      name: name.value,
+      phoneNumber: phoneNumber.value,
+      role: role.value,
+      status: status.value,
+    });
+  }
+};
 </script>
 <template>
   <v-btn @click="handleOpen" variant="flat" color="primary">회원추가</v-btn>
-  <v-dialog v-model="open" width="auto">
-    <v-card class="pa-2" max-width="200" title="회원 상태 변경">
-      <!-- <v-text-field v-model="email" label="이메일" /> -->
+  <v-dialog v-model="open" width="500px">
+    <v-card title="회원 추가">
+      <v-card-text>
+        <v-form ref="form">
+          <v-text-field v-model="email" :rules="rules.email" label="이메일" />
+          <v-text-field v-model="name" :rules="rules.text" label="이름" />
+          <v-text-field
+            v-model="phoneNumber"
+            :rules="rules.phoneNumber"
+            label="휴대폰 번호"
+          />
+          <v-select
+            v-model="role"
+            :items="roleItems"
+            item-value="value"
+            item-title="label"
+            :rules="[(v) => !!v || '권한을 선택해주세요.']"
+          />
+          <v-select
+            v-model="status"
+            :items="statusItems"
+            item-value="value"
+            item-title="label"
+            :rules="[(v) => !!v || '상태를 선택해주세요.']"
+          />
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="handleClose" variant="flat" color="error">취소</v-btn>
+        <v-btn @click="handleAddUser" variant="flat" color="primary"
+          >추가</v-btn
+        >
+      </v-card-actions>
     </v-card>
   </v-dialog>
 </template>
